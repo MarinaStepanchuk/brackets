0a379505ba90053f96151e642a986e0d7b5f210c
MarinaStepanchuk <mari20002@mail.ru>
2022-07-31 23:13:44 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let open = [];
+let close = [];
+
+bracketsConfig.forEach(value => {
+  open.push(value[0]);
+  close.push(value[1]);
+});
+
+let steck = [];
+
+for (let z = 0; z < str.length; z++) {
+  if (open.includes(str[z]) && steck.length === 0){
+    steck.push(str[z])
+  } else if (close.includes(str[z])) {
+        if (open.indexOf(steck[steck.length-1]) === close.indexOf(str[z])) {
+          steck.pop()
+        } else if(open.indexOf(str[z]) !== close.indexOf(str[z])) {return false} else {steck.push(str[z])}
+  } else {
+    steck.push(str[z])
+  }
+}
+
+return steck.length === 0;
 }
